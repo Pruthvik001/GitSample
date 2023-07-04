a54109032761b63177d88a2357dc66f676299b75
Pruthvi K <pruthvi.k@emudhra.com>
2023-07-04 10:57:08 +0530

    Revert "This is our fourth commit"
    This reverts commit 1e95ad6efdd869ea4595560c9a836f5f88fa45c6.

--- app/src/main/java/com/biometrics/gitsample/MainActivity.java
+++ app/src/main/java/com/biometrics/gitsample/MainActivity.java
@@ -12,6 +12,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         System.out.println("this is contained in my second commit");
-        bvsbbxv chbvfb vv
+        today is a good day
     }
 }
