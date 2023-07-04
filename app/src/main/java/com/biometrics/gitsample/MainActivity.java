91966926194118d72507a157d639aef8de135979
Pruthvi K <pruthvi.k@emudhra.com>
2023-07-04 12:12:48 +0530

    Change 2 commit

--- app/src/main/java/com/biometrics/gitsample/MainActivity.java
+++ app/src/main/java/com/biometrics/gitsample/MainActivity.java
@@ -12,6 +12,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         System.out.println("this is contained in my second commit");
-        System.out.println("Experimental");
+        System.out.println("Change 2");
     }
 }
