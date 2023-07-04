0a6b5140ddd37ec62984386c5e431e14aa93bb0f
Pruthvi K <pruthvi.k@emudhra.com>
2023-07-04 11:35:07 +0530

    Experimental commit

--- app/src/main/java/com/biometrics/gitsample/MainActivity.java
+++ app/src/main/java/com/biometrics/gitsample/MainActivity.java
@@ -12,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         System.out.println("this is contained in my second commit");
+        System.out.println("Experimental");
     }
 }
